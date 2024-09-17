3a2d4090761c50e56bee2569cb89b121dbfe7878
Alexander Leonard <71717066+adleonard96@users.noreply.github.com>
2024-09-17 17:37:26 +0000

    working on vscode debugging

--- expenseTracker/Program.cs
+++ expenseTracker/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Driver
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(args.ToString());
+
+    }
+}
