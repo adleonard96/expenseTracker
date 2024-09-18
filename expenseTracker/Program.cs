7685d86e7811f1104f9386a323a69de0ba17a69d
Alexander Leonard <71717066+adleonard96@users.noreply.github.com>
2024-09-18 16:56:59 +0000

    Added initial control statements

--- expenseTracker/Program.cs
+++ expenseTracker/Program.cs
@@ -2,7 +2,34 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(args.ToString());
-
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please enter one of the following commands:\n-add\n-list\n-summary\n-delete\n-budget\n-export\n");
+            return;
+        }
+        switch(args[0].ToLower()) 
+        {
+            case "add":
+                Console.WriteLine("Unimplimented");
+                break;
+            case "list":
+                Console.WriteLine("Unimplimented");
+                break;
+            case "summary":
+                Console.WriteLine("Unimplimented");
+                break;
+            case "delete":
+                Console.WriteLine("Unimplimented");
+                break;
+            case "budget":
+                Console.WriteLine("Unimplimented");
+                break;
+            case "export":
+                Console.WriteLine("Unimplimented");
+                break; 
+            default: 
+                Console.WriteLine(string.Format("Invalid argument of {0}", args[0]));
+                break;
+        }
     }
 }
